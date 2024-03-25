7e63ba0926e7f8294945aa2d5eb227d80d4b71d7
Pratyush Kamal <pratyushkamal88@gmail.com>
2024-03-25 14:25:27 +0530

    routing #4

--- BABEL/src/pages/home.jsx
+++ BABEL/src/pages/home.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
+// import React from 'react';
+// import Navbar from '../components/Navbar';
 
-const About = () => {
-  return(
+// const About = () => {
+//   return(
+//     <>
+//     <Navbar/>
+//     Hello from home.
+//     </>
+//   )
+// }
+// export default About;
+
+
+import React from 'react';
+import Navbar from "../components/Navbar";
+import image from "../assets/babel_home.png";
+import styles from './home.module.css';
+
+const home = () => {
+  return (
     <>
-    <Navbar/>
-    Hello from home.
+      <Navbar />
+      <div style={styles.spacer}>
+      </div>
+      <div className={styles.container}>
+
+        {/* Text and buttons div */}
+        <div style={{ flex: 2, marginLeft: '45px' }}>
+          <div>
+            <h1 className={`${styles.bold} ${styles.heading}`}>
+              <span className={styles.largeText}>BABEL</span>
+              <div className={styles.empty1}></div>
+            </h1>
+            <p className={`${styles.text} ${styles.believeText}`}> 
+            Welcome to Babel, where art and tech merge to transcend language barriers. Our AI revolutionizes translation, preserving meaning and emotion. Unlike traditional methods, we offer solutions for text, paragraphs, and phrases. Precision and innovation blend human expression with computational power. Babel marks a linguistic paradigm shift, elevating communication. Join us in unlocking language's true potential with technology.
+            </p>
+          </div>
+
+          <div style={{ marginTop: '50px' }}>
+            <button className={styles.button}>Test It Out</button>
+            <button className={styles.button}>Who Are We?</button>
+          </div>
+        </div>
+        {/* Image div */}
+        <div style={{ flex: 1 }} className={styles.imagecontainer}>
+          <img src={image} alt="The Food Barn" className={styles.image} />
+        </div>
+      </div>
+      
     </>
-  )
-}
-export default About;
+  );
+};
+
+export default home;
